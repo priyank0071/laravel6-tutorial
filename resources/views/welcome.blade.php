b72ea33529bfa6aacb94da0ef69e2c0228078469
Shahzad <shahzadali.infonic@gmail.com>
2021-02-11 13:06:14 +0530

    Revert "Laravel 6 Routing : Section 2"
    This reverts commit f4641169a49aac21379bc8a4d06f31bf5587eb2d.
    In this we include topics
    Basic routing and views,
    Pass request to views,
    Route Wildcard,
    Routing to controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laracasts
+                    Laravel
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/posts/{post}','PostsController@show');
